af5e14cd5c4fe1fd2206aef69e0fa3d0a08bea2c
maievstorm <38059981+maievstorm@users.noreply.github.com>
2022-06-17 01:25:44 +0700

    cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,6 +16,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://dpaportal.apps.xplat.fis.com.vn/']
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
