96edbe565ecd13a814712e0c90d9ef0bca5aa80d
maievstorm <38059981+maievstorm@users.noreply.github.com>
2022-06-29 15:03:29 +0700

    Update invoice.js

--- controllers/invoice.js
+++ controllers/invoice.js
@@ -21,10 +21,10 @@ async function getInvoice(page = 1) {
 
 async function createInvoice(body) {
     return db.tx(async t => {
-        const prod = await t.one("INSERT INTO dpzconf.invoice(item_name,item_type,customer_invoice_data, subscription_id, plan_history_id, invoice_period_start_date, invoice_period_end_date, invoice_description, invoice_amount, invoice_created_ts, invoice_due_ts, invoice_paid_ts) " +
+        const invoice = await t.one("INSERT INTO dpzconf.invoice(item_name,item_type,customer_invoice_data, subscription_id, plan_history_id, invoice_period_start_date, invoice_period_end_date, invoice_description, invoice_amount, invoice_created_ts, invoice_due_ts, invoice_paid_ts) " +
             "VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING id", [body.item_name,body.item_type,body.customer_invoice_data, body.subscription_id, body.plan_history_id, body.invoice_period_start_date, body.invoice_period_end_date, body.invoice_description, body.invoice_amount, body.invoice_created_ts, body.invoice_due_ts, body.invoice_paid_ts]);
         return {
-            prod
+            invoice
         }
     });
 }
@@ -60,8 +60,8 @@ async function getInvoiceBysubntype(subscription_id,item_type) {
 
 async function updateInvoice(id, body) {
     return db.task(async t => {
-        const data = await t.one("UPDATE pzconf.invoice SET customer_invoice_data=$1, invoice_description=$2 " +
-        " WHERE id=$5 RETURNING id", [body.customer_invoice_data, body.invoice_description, id]);
+        const data = await t.one("UPDATE pzconf.invoice SET customer_invoice_data=$2, invoice_description=$3 " +
+        " WHERE id=$1 RETURNING id", [id,body.customer_invoice_data, body.invoice_description]);
         
         return {
             data
