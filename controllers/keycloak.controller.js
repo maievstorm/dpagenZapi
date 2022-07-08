075fc0cf63f9e6c7b8ddc313ca40387d1d770d0f
vukhanh09 <vukhanh0920@gmail.com>
2022-07-08 14:32:38 +0700

    add

--- controllers/keycloak.controller.js
+++ controllers/keycloak.controller.js
@@ -1,12 +1,61 @@
-async function getAllUser(req,res){
-    try {
+const axios = require('axios').default;
 
-    }
-    catch (error) {
-        console.error(error);
-
-    }
+async function getInfo(req,res){
+    axios({
+        method:'get',
+        url:'https://streamdpa.apps.xplat.fis.com.vn/connectors'
+    })
+    .then(data=>{
+        return res.status(200).json({
+            data:data.data
+        })
+    })
 }
+
+async function getAllUser(req, res) {
+    let server = 'https://sso-fis-mbf-dplat.apps.xplat.fis.com.vn'
+    let realm = 'dpa'
+    const params = new URLSearchParams({
+        grant_type: 'password',
+
+        client_id: 'admin-cli',
+
+        username: 'hung',
+        password: '123456a@'
+
+    });
+    axios({
+        method: 'post',
+        url: `${server}/auth/realms/${realm}/protocol/openid-connect/token`,
+        // data: {
+        //     'client_id': 'admin-cli',
+        //     'grant_type': 'password',
+        //     'username': 'hung',
+        //     'password': '123456a@'
+        // },
+        data: params.toString(),
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive'
+        }
+    })
+        .then(data => {
+            return res.status(200).json({
+                data: data.data
+            })
+        })
+        .catch(error => {
+            console.log(error)
+            return res.status(200).json({
+                message: error
+            })
+        })
+}
+
+
 module.exports = {
-    getAllUser
+    getAllUser,
+    getInfo
 }
--- routes/keycloak.routes.js
+++ routes/keycloak.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUser } = require('../controllers/keycloak.controller.js');
+const { getAllUser,getInfo } = require('../controllers/keycloak.controller.js');
 
 router.get('/', getAllUser);
+router.get('/info', getInfo);
 
 module.exports = router;
